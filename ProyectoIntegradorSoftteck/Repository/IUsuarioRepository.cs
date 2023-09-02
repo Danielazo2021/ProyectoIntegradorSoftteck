ad3b78945c340887032358e87ff98f2b0be1c403
Pablo Ortiz <63486413+PabloOrtiz07@users.noreply.github.com>
2023-09-02 00:24:38 -0300

    Agrego repositorio usuario

---
+++ ProyectoIntegradorSoftteck/Repository/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoIntegradorSoftteck.Entities;
+
+namespace ProyectoIntegradorSoftteck.Repository
+{
+    public interface IUsuarioRepository
+    {
+        public interface IUsuarioRepository
+        {
+            Task<Usuario> Insertar(Usuario usuario);
+            Task<Usuario?> ObtenerPorId(int id);
+        }
+
+    }
+}
