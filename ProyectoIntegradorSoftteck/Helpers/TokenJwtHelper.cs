e5c66d5ee3260e22f4a25c65aa87490fa655a6f2
113493_Grecco_Daniel <113493@tecnicatura.frc.utn.edu.ar>
2023-09-11 23:21:27 -0300

    update UnitOfWork

---
+++ ProyectoIntegradorSoftteck/Helpers/TokenJwtHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ProyectoIntegradorSoftteck.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProyectoIntegradorSoftteck.Helpers
+{
+    public class TokenJwtHelper   // ver si falta algo mas...
+    {
+        private IConfiguration _configuration;
+        public TokenJwtHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GenerateToken(Usuario usuario)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]), // traemos el dato del json config
+                new Claim(ClaimTypes.NameIdentifier, usuario.CodUsuario.ToString()),
+                new Claim(ClaimTypes.Name , usuario.Nombre),
+
+
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var securityToken = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(securityToken); //devueve el token generado
+        }
+
+    }
+}
